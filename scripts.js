051792fc6f7981729072568ee945b29c820a9654
Blaazen <blqzen@gmail.com>
2025-08-24 22:10:40 +0200

    feat: slider added

--- scripts.js
+++ scripts.js
@@ -1,12 +1,17 @@
-<<<<<<< HEAD
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
-
+    "/"];
+    
+var slider = document.getElementById("myRange")
+var passLength = document.getElementById("length-output")
+    
+slider.addEventListener('input', function() {
+    passLength.textContent = this.value;
+})
 
 function GeneratePasswords() {
     pass1 = ""
     pass2 = ""
-    for (let i = 0; i < 15; i +=1 ) {
+    for (let i = 0; i < passLength; i +=1 ) {
         let char1 = characters[Math.floor(Math.random()* characters.length)]
         let char2 = characters[Math.floor(Math.random()* characters.length)]
         pass1 += char1
@@ -30,36 +35,3 @@ function copyOnClick(id) {
 
     alert("Copied the text: " + text)
 }
-=======
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
-
-
-function GeneratePasswords() {
-    pass1 = ""
-    pass2 = ""
-    for (let i = 0; i < 15; i +=1 ) {
-        let char1 = characters[Math.floor(Math.random()* characters.length)]
-        let char2 = characters[Math.floor(Math.random()* characters.length)]
-        pass1 += char1
-        pass2 += char2
-    }
-
-    pass1El = document.getElementById("pass1-el")
-    pass2El = document.getElementById("pass2-el")
-    
-    pass1El.textContent = pass1
-    pass2El.textContent = pass2
-}
-
-function copyOnClick(id) {
-
-    let element = document.getElementById(id)
-
-    let text = element.textContent
-
-    navigator.clipboard.writeText(text)
-
-    alert("Copied the text: " + text)
-}
->>>>>>> 879d9563fcfb9334b7e617dd6ba215e1d406b1f5
